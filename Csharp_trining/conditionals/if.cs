fb792e6f4d7369bdacd35a8276e80d740530b532
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-06-23 22:24:45 +0530

    conditional statements

---
+++ Csharp_trining/conditionals/if.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+class Conditionalstatements_if
+{
+    static void Main()
+    {
+        //conditional statements
+
+        //even or odd
+        int num = 5;
+        if (num % 2 == 0)
+        {
+            Console.WriteLine($"{num} is even number");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is odd number");
+        }
+
+
+
+        
+
+        int[] numbers = { 6, 10, 11, 5, 6 };
+        foreach (int number in numbers)
+        {
+            if (number == 10)
+            {
+                Console.WriteLine($"{number} is eqyal to 10");
+            }
+            else if(number > 10)
+            {
+                Console.WriteLine($"{number} is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is less than 10");
+            }
+        }
+
+
+
+        int[] Students_marks = { 100, 80, 90, 50, 20 };
+        foreach(int Student_marks in Students_marks)
+        {
+            if (Student_marks >= 90)
+            {
+                Console.WriteLine($" student got S-Grade");
+            }
+            else if (Student_marks >= 80)
+            {
+                Console.WriteLine($"student got A-Grade");
+            }
+            else if (Student_marks >= 70)
+            {
+                Console.WriteLine($"student got B-Grade");
+            }
+            else if (Student_marks >= 60)
+            {
+                Console.WriteLine($"student got C-Grade");
+            }
+            else if (Student_marks >= 50)
+            {
+                Console.WriteLine($"student got D-Grade");
+            }
+            else if (Student_marks >= 40)
+            {
+                Console.WriteLine($"student got E-Grade");
+            }
+            else
+            {
+                Console.WriteLine($"student is Fail");
+            }
+        }
+
+
+
+    }
+}
---
+++ Csharp_trining/conditionals/switch.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+
+class Conditionalstatements_switch
+{
+    static void Main()
+    {
+        //switch case
+
+        //day of the week
+        int day = 3;
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid day");
+                break;
+        }
+
+
+        //vowel or consonent
+        char ch = 'e';
+
+        switch (ch)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                Console.WriteLine("Vowel");
+                break;
+            default:
+                Console.WriteLine("Consonant");
+                break;
+        }
+
+
+        //month name from number 
+        int month = 4;
+
+        switch (month)
+        {
+            case 1:
+                Console.WriteLine("January");
+                break;
+            case 2:
+                Console.WriteLine("February");
+                break;
+            case 3:
+                Console.WriteLine("March");
+                break;
+            case 4:
+                Console.WriteLine("April");
+                break;
+            case 5:
+                Console.WriteLine("May");
+                break;
+            case 6:
+                Console.WriteLine("June");
+                break;
+            default:
+                Console.WriteLine("Invalid month");
+                break;
+        }
+
+        //Grade based on marks
+        int marks = 86;
+
+        switch (marks / 10)
+        {
+            case 10:
+            case 9:
+                Console.WriteLine("S Grade");
+                break;
+            case 8:
+                Console.WriteLine("A Grade");
+                break;
+            case 7:
+                Console.WriteLine("B Grade");
+                break;
+            case 6:
+                Console.WriteLine("C Grade");
+                break;
+            case 5:
+                Console.WriteLine("D Grade");
+                break;
+            default:
+                Console.WriteLine("Fail");
+                break;
+        }
+    }
+}
