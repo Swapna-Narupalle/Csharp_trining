873b9433ea694dc46231bf1e15d35fa352102219
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-06-30 18:09:35 +0530

    task methods

--- Csharp_trining/Task/task1.cs
+++ Csharp_trining/Task/task1.cs
@@ -55,6 +55,103 @@ class Task
 
 
         //10.Diff b/w for and foreach ? Explain with example
+        ///<summary>
+        ///for loop: Used to iterate with a counter and index-based access.
+        ///foreach loop:Used to iterate over a collection without using an index.
+        ///</summary>
+        //get employee names by using for loop
+        for (int i=0; i<empNames.Length; i++)
+        {
+            Console.WriteLine($"employee Name is: {empNames[i]}");
+        }
+        //get employee names by using foreach loop
+        foreach(string empName in empNames)
+        {
+            Console.WriteLine($"employee is: {empName}");
+        }
 
+
+        //11.Explain me what is the importance of the Looping system. ? Explain with Example
+        ///<summary>
+        ///Loops: loop iterates until the condition is false
+        ///while loop:it iterates the block until the condition is true
+        ///do-while loop: Executes the block of code at least once, and repeats until the condition is true.
+        ///</summary>
+        //example of while loop
+        string Name = "Swapna";
+        while (Name.EndsWith("a"))
+        {
+            Console.WriteLine($"{Name} ends with 'a' letter");
+            Name = Name.Substring(0, Name.Length - 1);
+        }
+        //example by using do-while loop
+        string employee_Name;
+        do
+        {
+            Console.WriteLine("enter the employee Name(min 6 letters)");
+            employee_Name = Console.ReadLine();
+        } while (employee_Name.Length < 6);
+        Console.WriteLine($"employee name is {employee_Name}");
+
+
+        //12.What are the floating data types ? EXample
+        ///<summary>
+        ///floting data types are used to store the decimal values
+        ///Example:decimal, float, double
+        ///</summary>
+        decimal price = 23.22m;
+        float temperature = 34.99f;
+        double distance = 78.998;
+        Console.WriteLine($"decimal price is: {price}");
+        Console.WriteLine($"float temperature is: {temperature}");
+        Console.WriteLine($"double distance is: {distance}");
+
+
+        //13.What are the Logical operators importance of that ? Explain with example
+        ///<summary>
+        ///logical operators:&&, ||, !
+        ///logical operators: By using logical operators we can find whether the given codition is true or false. These are used in conditional statements
+        ///like if, else if, else
+        ///</summary>
+        Console.WriteLine("enter age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        bool hasVoterid = false;
+        if(age>=18 && !hasVoterid)
+        {
+            Console.WriteLine("eligible to vote");
+        }
+        else if(age>=18 || !hasVoterid)
+        {
+            Console.WriteLine("Partially eligible to vote");
+        }
+        else
+        {
+            Console.WriteLine("Not eligible to vote");
+        }
+
+
+        //14.What is the nullable types ? Example
+        ///<summary>
+        ///nullable data types:A nullable type allows a value type (like int, bool, double, etc.) to also hold a null value.
+        ///</summary>
+        int? age1 = null;
+        if(age1 == null)
+        {
+            Console.WriteLine("age is not provided");
+        }
+        else
+        {
+            Console.WriteLine($"age is: {age1}");
+        }
+
+
+        //15.What is implicit conversation type ? Example
+        ///<summary>
+        ///implicit conversion: to convert value of small data type into large data type
+        ///</summary>
+        byte experience = 18;
+        int updatedexp = experience;
+        Console.WriteLine(updatedexp);
+        Console.WriteLine(updatedexp.GetType());
     }
 }
---
+++ Csharp_trining/Task/task1methods.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+class Task1_methods
+{
+    static void Main()
+    {
+       string stuDetails = GetstudentMarks("Swapna", 99);
+       Console.WriteLine(stuDetails);
+
+        Checknumbertype();
+
+        GateExamfeeDetails();
+
+        string fooddetails = GetfoodOrderdetails();
+        Console.WriteLine(fooddetails);
+    }
+
+    //16.Prepare the Method with params with return types.
+    static string GetstudentMarks(string stuName, int marks)
+    {
+        return $"{stuName} secured {marks} marks";
+    }
+
+
+    //17.Prepare the Methods with conditions (if elseif else)
+    static void Checknumbertype()
+    {
+        Console.WriteLine("enter a number:");
+        int num = int.Parse(Console.ReadLine());
+        if (num > 0)
+        {
+            Console.WriteLine($"{num} is positive number");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine($"{num} is negative number");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is zero");
+        }
+
+    }
+
+
+    //18.Prepare the methods with Conditions (switch case)
+    static void GateExamfeeDetails()
+    {
+        Console.WriteLine("enter the gender(Male or Female)");
+        string gender = Console.ReadLine().Trim().ToLower();
+        int? examfee = null;
+        switch (gender)
+        {
+            case "male":
+                examfee = 1800;
+                break;
+            case "female":
+                examfee = 900;
+                break;
+            default:
+                Console.WriteLine("invalid input, pls enter either Male or Female");
+                break;
+        }
+        if (examfee.HasValue)
+        {
+            Console.WriteLine($"for {gender} gate exam fee is: {examfee}");
+        }
+    }
+
+
+    //19.Prepare the Methods with if else if else and Switch case conditions.
+    static string GetfoodOrderdetails()
+    {
+        Console.WriteLine("select your food item: Gobi, chicken biryani, chilly chicken");
+        string selectedorder = Console.ReadLine();
+        double price;
+        string orderDetails;
+        switch (selectedorder.ToLower())
+        {
+            case "gobi":
+                price = 89.67;
+                break;
+            case "chicken biryani":
+                price = 256.78;
+                break;
+            case "chilly chicken":
+                price = 120.56;
+                break;
+            default:
+                return $"invalid food item is selected";
+                break;
+        }
+        if (price < 100)
+        {
+            orderDetails = "This is a budget-friendly item";
+        }
+        else if(price>=100 && price <= 200)
+        {
+            orderDetails = "This is a mid-range item";
+        }
+        else
+        {
+            orderDetails = "this is a premium item";
+        }
+        return $"you ordered: {selectedorder}\n price: {price} \n orderDetails: {orderDetails}";
+    }
+}
